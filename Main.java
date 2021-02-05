02193167b89a22ee73ce4b928478553a40ae7567
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2021-02-04 21:44:25 -0600

    Actualizacion calculadora

--- Main.java
+++ Main.java
@@ -9,9 +9,9 @@ class Main{
 public static void main(String[] args) {
 
     Vector vec = new Vector();
-    System.out.println("ok");
+    //System.out.println("ok");
     vec.decode("Ejercicio 2 AED\\datos.txt");
-    System.out.println("ok");
+    //System.out.println("ok");
     
 
 
--- Vector.java
+++ Vector.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 class Vector implements Calculadora{
     public Vector(){
 
-    Stack<Integer> stack=new Stack<>();
+        Stack<Integer> calc = new Stack<Integer>();
 }
 String line = "No hay archivo";
 
@@ -18,16 +18,61 @@ public String decode(String a){
     BufferedReader br = null;
     String line = "No hay archivo";
     try{
-        FileReader fr = new FileReader(a);
+        FileReader fr = new FileReader(a); // lee el archivo
         br = new BufferedReader(fr);
+        //por cada linea del txt se hace un loop
         while((line = br.readLine())!= null){
+            //imprime la linea del txt
             System.out.println(line);
         }
-    }catch(IOException e){
+    }catch(IOException e){ //si hay una exepcion entonces el catch hace que no se cierre el programa
         System.out.println("No");
     }
+    //regresa la linea del txt o el mensaje de error
     return line;
 }
-
+public int operar(stack b){
+    sc = b;
+    String input = sc.next();
+    Stack<Integer> calc = new Stack<Integer>();
+    while(sc.hasNext()){
+    for(int i = 0; i < input.length(); i++){
+        char c = input.charAt(i);
+        int x = 0;
+        int y = 0;
+        int r = 0;
+        if(Character.isDigit(c)){
+        int t = Character.getNumericValue(c);
+            calc.push(t);
+        }
+        else if(c == '+'){
+            x = calc.pop();
+            y = calc.pop();
+            r = x+y;
+            calc.push(r);
+        }
+        else if(c == '-'){
+            x = calc.pop();
+            y = calc.pop();
+            r = x-y;
+            calc.push(r);
+        }
+        else if(c == '*'){
+            x = calc.pop();
+            y = calc.pop();
+            r = x*y;
+            calc.push(r);
+        }
+        else if(c == '/'){
+            x = calc.pop();
+            y = calc.pop();
+            r = x/y;
+            calc.push(r);
+        }
+}
+ }
+    int wow = calc.pop();
+    System.out.println(wow);
+}
 
 }
